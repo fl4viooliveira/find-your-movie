e9579618c5d35529a8bd9124cd82d2970f02c514
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-01 16:55:37 +0000

    Call the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ var h1 = document.createElement("h1");
 
 h1.textContent = "Test from script.js file";
 test.append(h1);
+
+// console.log(youtubeKey)
+
+var key = youtubeKey;
+var input = "The Matrix"
+var queryURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${input}&key=${key}`
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+
+    // Log the queryURL
+    console.log(response);
+  })
