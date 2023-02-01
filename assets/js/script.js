f12e732ea8268abccfcc5e97219cb2fdc4b3f0b9
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-01 22:12:17 +0000

    Create API Call to VideoId
    Add a test iframe using API videoId

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,50 @@
-var key = youtubeKey;
-var input = "The Matrix"
-var queryURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${input}&key=${key}`
+var youtube = $("#youtube");
+youtube.attr("class", "container m-3");
+var h2 = $("<h2>");
+h2.text("Add a movie name to test:");
 
+var button = $('<button class="btn btn-primary m-3">Search</button>');
+
+var input = $("<input>");
+youtube.append(h2);
+youtube.append(input);
+youtube.append(button);
+
+// input.val;
+//
+var input = "avatar trailer";
+
+function movieCall(inp) {
   $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-
+    url: `${YOUTUBE_URL}5&q=${inp}&key=${YOUTUBE_KEY}`,
+    method: "GET",
+  }).then(function(response) {
     console.log(response);
-  })
+    var videoId = response.items[0].id.videoId;
+
+    var iframe = $(`
+      <iframe width="560" height="315" 
+      src="https://www.youtube.com/embed/${videoId}" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; 
+      autoplay; 
+      clipboard-write; 
+      encrypted-media; 
+      gyroscope; 
+      picture-in-picture; 
+      web-share" 
+      allowfullscreen></iframe>
+      `)
+
+    youtube.append(iframe)
+  });
+}
+// movieCall(input)
+
+
+button.on('click', function(event) {
+  event.preventDefault()
+
+  movieCall(`${input.val}`);
+})
