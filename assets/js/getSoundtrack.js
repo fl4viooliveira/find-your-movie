73425d87be852a5555318fe31f6aa502986eda08
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-02-02 18:47:22 +0200

    draft cards made for each album

--- assets/js/getSoundtrack.js
+++ assets/js/getSoundtrack.js
@@ -8,9 +8,9 @@ var movie = "Harry Potter"
 
 // Keep below!!!
 var movieArr = movie.split(' ');
-console.log(movieArr);
+// console.log(movieArr);
 var movieURLInput = movieArr.join('+');
-console.log(movieURLInput);
+// console.log(movieURLInput);
 
 var movieQueryURL = "https://itunes.apple.com/search?&entity=album&term=" + movieURLInput + "&limit=5";
 
@@ -24,17 +24,35 @@ $.ajax({
         for (let i = 0; i < 5; i++) {
             // console.log("loop is working")
             var thisArray = array.results[i];
+            var card = $('<div>').addClass("card m-3 p-2");
             // console.log(thisArray);
-            var albumCover = array.results[i].artworkUrl100;
+
+            // Get the album cover
+            var albumCover = thisArray.artworkUrl100;
+            var albumIcon = $('<img>').attr("src", albumCover).css({ "height": 50, "width": 50 })
             console.log("The album cover source is :" + albumCover);
-            var albumName = array.results[i].collectionCensoredName;
+
+            // Get the Album name
+            var albumName = thisArray.collectionCensoredName;
             console.log("This album is called: " + albumName);
-            var artist = array.results[i].artistName;
+            var albumTitle = $('<h5>').addClass("card-title").text(albumName);
+
+            // Get artist information
+            var artist = thisArray.artistName;
             console.log("The artist is: " + artist);
-            var trackNumber = array.results[i].trackCount;
+            var artistName = $('<p>').text("Artist: " + artist);
+
+            // Get the number of tracks
+            var trackNumber = thisArray.trackCount;
             console.log("There are " + trackNumber + " tracks in this album");
-            var albumURL = array.results[i].collectionViewUrl;
-            console.log("The link to this album is: " + albumURL)
-            console.log("________________________")
+            var numOfTracks = $('<p>').text("Number of tracks: " + trackNumber);
+
+            // Get the link to the album
+            var albumURL = thisArray.collectionViewUrl;
+            console.log("The link to this album is: " + albumURL);
+            var linkToAlbum = $('<p>').text("Link to ITunes album: " + albumURL)
+
+            console.log("_____________________________________________________________")
+            $("#music-input").append(card.append(albumTitle, albumIcon, artistName, numOfTracks, linkToAlbum))
         }
     })
