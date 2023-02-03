d5e09c350acc52d633cd14e2fab547d0d1922d7b
Mister Stan <lucianstan87@yahoo.com>
2023-02-03 14:52:53 +0000

    add the OMDb API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 // OMDb api call
-var test = document.querySelector(".test");
-var h1 = document.createElement("h1");
-
-h1.textContent = "Test from script.js file";
-test.append(h1);
-
 $(document).ready(function () {
+
   // search button click
-  $("#search-button").click(function () {
-    // get movie title from input
+  $("#search-button-new").on("click", function () {
+
+    //get movie title from input
     var movieTitle = $("#search-input").val();
 
-    // search for the movie title in OMDb API and appends it in the html
+    //search for the movie title in OMDb API and appends it in the html
     searchMovie(movieTitle);
+
   });
 });
 
@@ -20,19 +17,29 @@ function searchMovie(movieTitle) {
 
   // AJAX call to OMDb API
   $.ajax({
-    type: "GET",
     url: `https://www.omdbapi.com/?t=${movieTitle}&apikey=556cb440`,
+    method: "GET",
     success: function (movieInfo) {
-      // check if movie information was successfully retrieved
+console.log(movieInfo);      
+
+// check if the movie information was successfully retrieved
       if (movieInfo.Response === "True") {
-        // create movie container
-        var movieContainer = $("<div>").addClass("movie-container");
-        var movieTitle = $("<h2>").text(movieInfo.Title);
-        var movieYear = $("<p>").text(movieInfo.Year);
-        var moviePoster = $("<img>").attr("src", movieInfo.Poster);
+        
+                    // create movie container
+        var movieContainer = $("<div>").addClass("card");
+
+        var moviePoster = $("<img>").attr("src", movieInfo.Poster).addClass("card-img-top");
+
+        var movieData = $("<div>").addClass("card-body");
+        var movieTitle = $("<h2>").addClass("card-title").text(movieInfo.Title);
+        var movieYear = $("<h3>").addClass("card-text").text(movieInfo.Year);
+        movieData.append(movieTitle, movieYear);
+
         // append movie information to movie container
-        movieContainer.append(movieTitle, movieYear, moviePoster);
-        $("#movies").html(movieContainer);
+        movieContainer.append(moviePoster, movieData);
+        $("#section2").html(movieContainer);
+        window.location.href = "#section2";
+
       } else {
         alert("Please insert a valid movie title");
       }
@@ -43,53 +50,4 @@ function searchMovie(movieTitle) {
   });
 }
 // OMDb API call
-
-=======
-// YouTube Integration Test ---------------------------------------------
-var youtube = $("#youtube");
-youtube.attr("class", "container m-3");
-var h2 = $("<h2>");
-h2.text("Add a movie name to test:");
-
-var button = $('<button class="btn btn-primary m-3">Search</button>');
-
-var input = $("<input>");
-input.attr("id", "search");
-youtube.append(h2);
-youtube.append(input);
-youtube.append(button);
-
-function movieCall(inp) {
-  $.ajax({
-    url: `${YOUTUBE_URL}5&q=${inp}&key=${YOUTUBE_KEY_2}`,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    var videoId = response.items[0].id.videoId;
-
-    var iframe = $(`
-      <iframe width="560" height="315" 
-      src="https://www.youtube.com/embed/${videoId}" 
-      title="YouTube video player" 
-      frameborder="0" 
-      allow="accelerometer; 
-      autoplay; 
-      clipboard-write; 
-      encrypted-media; 
-      gyroscope; 
-      picture-in-picture; 
-      web-share" 
-      allowfullscreen></iframe>
-      `);
-
-    youtube.append(iframe);
-  });
-}
-
-button.on("click", function (event) {
-  event.preventDefault();
-  var inputMovie = $("#search").val();
-  movieCall(inputMovie);
-});
-// YouTube Integration Test ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
