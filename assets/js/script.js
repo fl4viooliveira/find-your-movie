f92719ce1a0abaed1031b159dd7f5ddf101d9fdc
Mister Stan <lucianstan87@yahoo.com>
2023-02-04 17:39:59 +0000

    add omdb api last updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // OMDb api call
-
 $(document).ready(function () {
 
   // search button click
@@ -21,20 +20,28 @@ function searchMovie(movieTitle) {
     url: `https://www.omdbapi.com/?t=${movieTitle}&apikey=556cb440`,
     method: "GET",
     success: function (movieInfo) {
-console.log(movieInfo);      
-
-// check if the movie information was successfully retrieved
+      // check if the movie information was successfully retrieved
       if (movieInfo.Response === "True") {
         
-                    // create movie container
+
+        console.log(movieInfo);
+        // create movie container
         var movieContainer = $("<div>").addClass("card");
 
         var moviePoster = $("<img>").attr("src", movieInfo.Poster).addClass("card-img-top");
 
         var movieData = $("<div>").addClass("card-body");
-        var movieTitle = $("<h2>").addClass("card-title").text(movieInfo.Title);
-        var movieYear = $("<h3>").addClass("card-text").text(movieInfo.Year);
-        movieData.append(movieTitle, movieYear);
+          var movieTitle = $("<h3>").addClass("card-title").text(movieInfo.Title);
+          var movieYear = $("<h3>").addClass("card-text").text(`Released: ${movieInfo.Released}`);
+          var runtime = $("<h3>").addClass("card-text").text(`Runtime: ${movieInfo.Runtime}`);
+          var movieActors = $("<h3>").addClass("card-text").text(`Stars: ${movieInfo.Actors}`);
+          var movieRating = $("<h3>").addClass("card-text").text(`Imdb: ${movieInfo.imdbRating} stars`);
+          var oscarsPrizes = $("<h3>").addClass("card-text");
+            var oscarsPrizesText = $("<small>").addClass("text-muted").text(`Awards: ${movieInfo.Awards}`);
+            oscarsPrizes.append(oscarsPrizesText);  
+          var plotDescription = $("<p>").addClass("card-description").text(movieInfo.Plot);
+
+        movieData.append(movieTitle, movieYear, runtime, movieActors, movieRating, oscarsPrizes, plotDescription,);
 
         // append movie information to movie container
         movieContainer.append(moviePoster, movieData);
@@ -52,3 +59,51 @@ console.log(movieInfo);
 }
 // OMDb API call
 
+/*
+var youtube = $("#youtube");
+youtube.attr("class", "container m-3");
+var h2 = $("<h2>");
+h2.text("Add a movie name to test:");
+
+var button = $('<button class="btn btn-primary m-3">Search</button>');
+
+var input = $("<input>");
+input.attr("id", "search");
+youtube.append(h2);
+youtube.append(input);
+youtube.append(button);
+
+function movieCall(inp) {
+  $.ajax({
+    url: `${YOUTUBE_URL}5&q=${inp}&key=${YOUTUBE_KEY_2}`,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var videoId = response.items[0].id.videoId;
+
+    var iframe = $(`
+      <iframe width="560" height="315" 
+      src="https://www.youtube.com/embed/${videoId}" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; 
+      autoplay; 
+      clipboard-write; 
+      encrypted-media; 
+      gyroscope; 
+      picture-in-picture; 
+      web-share" 
+      allowfullscreen></iframe>
+      `);
+
+    youtube.append(iframe);
+  });
+}
+
+button.on("click", function (event) {
+  event.preventDefault();
+  var inputMovie = $("#search-input").val();
+  movieCall(inputMovie);
+});
+// YouTube Integration Test ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+*/
