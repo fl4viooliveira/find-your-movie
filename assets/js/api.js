2516a06e54291ba3fa8b66b151810b8b7e8f9564
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-02-04 22:47:27 +0000

    Add itunes api to the project

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,15 +2,61 @@ var section = $("#movie-info");
 var getInput = $("#search-input");
 var inputBtn = $("#btn-search");
 
+// ITUNES API BLOCK ------------------------------------
+function soundTrack(inp) {
+  var movieArr = inp.split(" ");
+  var movieURLInput = movieArr.join("+");
+  var movieQueryURL =
+    "https://itunes.apple.com/search?&entity=album&term=" +
+    movieURLInput +
+    "&limit=5";
+  var footer = $("#footer-info");
+
+  $.ajax({
+    url: movieQueryURL,
+    method: "GET",
+  }).then(function (response) {
+    var array = JSON.parse(response);
+    console.log(array);
+
+    for (let i = 0; i < 5; i++) {
+      var thisArray = array.results[i];
+      var card = $("<div>");
+      console.log(thisArray);
+
+      // Get the album cover
+      var albumCover = thisArray.artworkUrl100;
+      var albumIcon = $("<img>").attr("src", albumCover);
+
+      // Get the Album name
+      var albumName = thisArray.collectionCensoredName;
+      var albumTitle = $('<p class="album-name">').text(albumName);
+
+      // Get the link to the album
+      var albumURL = thisArray.collectionViewUrl;
+      var linkToAlbum = $("<p>").text("Link to ITunes album: " + albumURL);
+
+      var anchor = $(`<a href=${albumURL} target="_blank">`);
+      anchor.append(albumIcon, albumTitle);
+
+
+      // $("#music-input").append(card.append(albumTitle, albumIcon, artistName, numOfTracks, linkToAlbum))
+      // footer.append(card.append(albumTitle, anchor));
+      footer.append(card.append(anchor));
+    }
+  });
+}
+// ITUNES API BLOCK ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 // YOUTUBE API BLOCK --------------------------------------
 function trailerCall(inp) {
   $.ajax({
-    url: `${YOUTUBE_URL}5&q=${inp}&key=${YOUTUBE_KEY}`,
+    url: `${YOUTUBE_URL}5&q=${inp}&key=${YOUTUBE_KEY_2}`,
     method: "GET",
   }).then(function (response) {
     console.log(response);
     var videoId = response.items[0].id.videoId;
-      // <iframe width="560" height="315" 
+    // <iframe width="560" height="315"
     var iframe = $(`
       <iframe 
       class="video"
@@ -28,7 +74,7 @@ function trailerCall(inp) {
       </iframe>
       `);
 
-    $('#trailer').append(iframe);
+    $("#trailer").append(iframe);
   });
 }
 // YOUTUBE API BLOCK ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -77,8 +123,8 @@ function movieSearch(inp) {
 
       infoAnchor.append(genre, actors, director, writer, overview, awards);
 
-     trailerCall(`${movieInfo.Title} ${movieInfo.Year} official trailer`) 
-
+      trailerCall(`${movieInfo.Title} ${movieInfo.Year} official trailer`);
+      soundTrack(movieInfo.Title);
     },
   });
 }
@@ -91,5 +137,6 @@ inputBtn.on("click", function (event) {
 
   movieSearch(movie);
 });
-// movieSearch("Avatar");
+
+// movieSearch("Harry Potter");
 // SEARCH BUTTON BLOCK ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
